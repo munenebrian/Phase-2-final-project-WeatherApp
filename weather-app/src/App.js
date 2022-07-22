1982f36ec0b59b05e91cfec71204e924bfd7d619
munenebrian <njerubrianmunene@gmail.com>
2022-07-22 12:44:22 +0300

    initializing my phase two project!

---
+++ weather-app/src/App.js
@@ -0,0 +1,16 @@
+import React, {useStae} from "react";
+import axios from "axios";
+
+function App() {
+ // const url = `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=6d8edebf6ae0e79385fd677f52dccb28`
+
+
+
+  return (
+    <div className="App">
+      home
+    </div>
+  );
+}
+
+export default App;
