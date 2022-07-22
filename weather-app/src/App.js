1b591a81fb7d33b325e2361f631f83568a94a152
munenebrian <njerubrianmunene@gmail.com>
2022-07-22 13:08:21 +0300

    starting up!

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,8 +1,8 @@
-import React, {useStae} from "react";
+import React, {useState} from "react";
 import axios from "axios";
 
 function App() {
- // const url = `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=6d8edebf6ae0e79385fd677f52dccb28`
+ // const url = `https://api.openweathermap.org/data/2.5/weather?q={city name}&appid=6d8edebf6ae0e79385fd677f52dccb28`
 
 
 
