c6bcd151c6f843f4f8191bfe36fadda5da33dda9
munenebrian <njerubrianmunene@gmail.com>
2022-07-22 13:44:10 +0300

    making changes to the src file!

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,4 +1,5 @@
 import React, {useState} from "react";
+import Container from "./Container";
 import axios from "axios";
 
 function App() {
@@ -7,8 +8,31 @@ function App() {
 
 
   return (
-    <div className="App">
-      home
+    <div className="app">
+      <div className="container">
+       <div className="top">
+        <div className="location">
+         <p>Nairobi</p>
+        </div>
+        <div className="temp">
+          <h1>20°F</h1>
+        </div>
+        <div className="description">
+          <p>Clouds</p>
+        </div>
+       </div>
+       <div className="bottom">
+        <div className="feels">
+          <p>20°F</p>
+        </div>
+        <div className="humidity">
+          <p>20%</p>
+        </div>
+        <div className="wind">
+          <p>12mph</p>
+        </div>
+       </div>
+      </div>
     </div>
   );
 }
